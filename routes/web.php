f40f134176b41b87ca9483f5e4fed3e2bbad72bc
Krystian Duma <git@krystian.duma.sh>
2019-11-15 01:24:18 +0100

    Up

--- routes/web.php
+++ routes/web.php
@@ -23,10 +23,18 @@ Route::get('/positions', function (\Illuminate\Http\Request $request) {
     return \App\Http\Resources\PositionResource::collection(\App\Position::latest('time')->with('Locator')->paginate(100));
 });
 Route::get('/position', function (\Illuminate\Http\Request $request) {
-    $pos = \App\Position::latest('time')->with('Locator')->first();
+    $positions = \App\Position::latest('time')->with('Locator')->take(99)->get();
+    $pos = $positions->first();
+    
+    $counter = 1;
+    $pins = $positions->map(function (\App\Position $position) use (&$counter) {
+        $pin_class = ($position->is_from_gsm ? 'pm2wtm' : 'pm2rdm').$counter++;
+        return "{$position->longitude},{$position->latitude},{$pin_class}";
+    })->reverse()->implode('~');
+        
     return response(<<<HTML
 <body style="text-align: center">
-<img style="max-width: 100%" width="650" src="https://static-maps.yandex.ru/1.x/?lang=en-US&ll={$pos->longitude},{$pos->latitude}&z=14&l=map,trf&size=650,450&pt={$pos->longitude},{$pos->latitude},pm2rdm" alt="Yandex Map of {$pos->longitude},{$pos->latitude}">
+<img style="max-width: 100%" width="650" src="https://static-maps.yandex.ru/1.x/?lang=en-US&ll={$pos->longitude},{$pos->latitude}&z=12&l=map,trf&size=650,450&pt={$pins}" alt="Yandex Map of {$pos->longitude},{$pos->latitude}">
 </body>
 HTML
     );
